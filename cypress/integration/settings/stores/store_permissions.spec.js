0a49c62a676c30f8f265be43fa3b9260a740b17d
mattbrailsford <me@mattbrailsford.com>
2020-08-31 14:30:50 +0100

    Added store permission based tests

---
+++ cypress/integration/settings/stores/store_permissions.spec.js
@@ -0,0 +1,190 @@
+context('Stores', () => {
+
+    let storeId;
+    let storeName;
+
+    beforeEach(() => {
+        cy.task('app:seed', ['clean', 'permissions']);
+        cy.task('app:clearCache');
+        cy.umbracoLogin(Cypress.env('username'), Cypress.env('password'));
+        cy.fixture('test-store').then(data => {
+            cy.umbracoApiRequest("/umbraco/backoffice/vendr/Store/SaveStore", "POST", data).then(store => {
+                storeId = store.id;
+                storeName = store.name;
+            });
+        });
+    });
+
+    it('can allow access to store by user group', () => {
+
+        // Go to commerce section
+        cy.get('[data-element="section-commerce"] a').click();
+
+        // Ensure we don't currently have access to a store
+        cy.get(`[data-element="tree-item-${storeName}"]`).should('not.be.visible');
+
+        // Got to settings section
+        cy.umbracoSection('settings');
+        cy.get('li .umb-tree-root:contains("Commerce")').should("be.visible");
+
+        // Open the store
+        cy.umbracoTreeItem("settings", ["Vendr", "Stores", storeName]).click();
+
+        // Go to permissions tab
+        cy.get('[data-element="sub-view-permissions"]').click();
+
+        // Give store access to Commerce user group
+        cy.get('.umb-property[label="User Groups"] .vendr-toggle').contains('Commerce').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Go to commerce section
+        cy.get('[data-element="section-commerce"] a').click();
+
+        // Reload the section
+        cy.reload(true);
+
+        // Ensure we don't currently have access to a store
+        cy.get(`[data-element="tree-item-${storeName}"]`).should('be.visible');
+
+    });
+
+    it('can allow access to store by user', () => {
+
+        // Go to commerce section
+        cy.get('[data-element="section-commerce"] a').click();
+
+        // Ensure we don't currently have access to a store
+        cy.get(`[data-element="tree-item-${storeName}"]`).should('not.be.visible');
+
+        // Got to settings section
+        cy.umbracoSection('settings');
+        cy.get('li .umb-tree-root:contains("Commerce")').should("be.visible");
+
+        // Open the store
+        cy.umbracoTreeItem("settings", ["Vendr", "Stores", storeName]).click();
+
+        // Go to permissions tab
+        cy.get('[data-element="sub-view-permissions"]').click();
+
+        // Give store access to Commerce user group
+        cy.get('.umb-property[label="Users"] .vendr-toggle').contains('admin').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Go to commerce section
+        cy.get('[data-element="section-commerce"] a').click();
+
+        // Reload the section
+        cy.reload(true);
+
+        // Ensure we don't currently have access to a store
+        cy.get(`[data-element="tree-item-${storeName}"]`).should('be.visible');
+
+    });
+
+    it.only('can toggle access to store for all user groups', () => {
+
+        // Got to settings section
+        cy.umbracoSection('settings');
+        cy.get('li .umb-tree-root:contains("Commerce")').should("be.visible");
+
+        // Open the store
+        cy.umbracoTreeItem("settings", ["Vendr", "Stores", storeName]).click();
+
+        // Go to permissions tab
+        cy.get('[data-element="sub-view-permissions"]').click();
+
+        // Give access to all groups
+        cy.get('.umb-property[label="User Groups"] .vendr-toggle:first-child').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Close the notification
+        cy.umbracoSuccessNotification().find('.close').click();
+
+        // Ensure all checkboxes are checked
+        cy.get('.umb-property[label="User Groups"] .vendr-toggle .umb-toggle').each($toggle => {
+            cy.wrap($toggle).should('have.class', 'umb-toggle--checked');
+        });
+
+        // Give access to all groups
+        cy.get('.umb-property[label="User Groups"] .vendr-toggle:first-child').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Close the notification
+        cy.umbracoSuccessNotification().find('.close').click();
+
+        // Ensure all checkboxes are checked
+        cy.get('.umb-property[label="User Groups"] .vendr-toggle .umb-toggle').each($toggle => {
+            cy.wrap($toggle).should('not.have.class', 'umb-toggle--checked');
+        });
+
+    });
+
+    it('can toggle access to store for all users', () => {
+
+        // Got to settings section
+        cy.umbracoSection('settings');
+        cy.get('li .umb-tree-root:contains("Commerce")').should("be.visible");
+
+        // Open the store
+        cy.umbracoTreeItem("settings", ["Vendr", "Stores", storeName]).click();
+
+        // Go to permissions tab
+        cy.get('[data-element="sub-view-permissions"]').click();
+
+        // Give access to all users
+        cy.get('.umb-property[label="Users"] .vendr-toggle:first-child').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Close the notification
+        cy.umbracoSuccessNotification().find('.close').click();
+
+        // Ensure all checkboxes are checked
+        cy.get('.umb-property[label="Users"] .vendr-toggle .umb-toggle').each($toggle => {
+            cy.wrap($toggle).should('have.class', 'umb-toggle--checked');
+        });
+
+        // Give access to all users
+        cy.get('.umb-property[label="Users"] .vendr-toggle:first-child').click();
+
+        // Save the changes
+        cy.get('[data-element="editor-footer"] .btn-success').click();
+
+        // Notification message should display
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Close the notification
+        cy.umbracoSuccessNotification().find('.close').click();
+
+        // Ensure all checkboxes are checked
+        cy.get('.umb-property[label="Users"] .vendr-toggle .umb-toggle').each($toggle => {
+            cy.wrap($toggle).should('not.have.class', 'umb-toggle--checked');
+        });
+
+    });
+
+});
